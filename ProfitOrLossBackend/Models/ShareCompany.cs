25c9408dc921fd9f69246a3a793ff5fdf4a71a17
prahlad574 <sivaprahlad@gmail.com>
2024-10-04 23:20:45 +0530

    added Db Migrations and modified classes by adding data annotation attributes

--- ProfitOrLossBackend/Models/ShareCompany.cs
+++ ProfitOrLossBackend/Models/ShareCompany.cs
@@ -1,10 +1,18 @@
 ﻿namespace ProfitOrLossBackend.Models
 {
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class ShareCompany
     {
+        [Column("ShareCompanyId")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         [Key]
         public int ShareCompanyId { get; set; }
+
+        [Column("ShareCompanyName")]
+        [MaxLength(100)]
         public string? ShareCompanyName { get; set; }
 
     }
