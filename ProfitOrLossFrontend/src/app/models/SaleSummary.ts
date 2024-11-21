145a83506244ba4595f17387844231c998cc8bc7
prahlad574 <sivaprahlad@gmail.com>
2024-11-21 23:49:28 +0530

    fixed live updates and ui alignment

---
+++ ProfitOrLossFrontend/src/app/models/SaleSummary.ts
@@ -0,0 +1,8 @@
+export interface SaleSummary{
+    shareCompany?: string;
+    costPrice?: number
+    sellingPrice?: number;
+    profitOrLoss?: number;
+    saleSummaryId: string;
+    financialYear: string;
+}
